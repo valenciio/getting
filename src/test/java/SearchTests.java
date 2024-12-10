61ac00cf118e666acabd8e60bc5644beaf60ff6c
Lenovo Gaming <selezenevva@krotech.ru>
2024-12-10 11:24:54 +0300

    SearchTest

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,14 +1,31 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SearchTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+    }
     @Test
-    void successfulSearchTest() {
+    void successfulSearchTest1() {
         open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+        $x("//textarea[@name='q']").setValue("selenide").pressEnter();
+        $x("//div[@id='search']").shouldHave(text("https://ru.selenide.org"));
+    }
+    @Test
+    void successfulSearchTest2() {
+        open("https://www.google.com/");
+        $x("//textarea[@name='q']").setValue("mos.ru").pressEnter();
+        $x("//div[@id='rcnt']").shouldHave(text("https://www.mos.ru"));
+    }
+    @Test
+    void successfulSearchTest3() {
+        open("https://ya.ru/");
+        $x("//input[@name='text']").setValue("mos.ru").pressEnter();
+        $x("//a[@href='https://www.mos.ru/']").shouldBe(visible);
     }
 }
