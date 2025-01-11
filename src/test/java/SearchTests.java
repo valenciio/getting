9dbe23d25bc54b138dbbdc50bb6b929eb4796e7f
valenciio <156203190+valenciio@users.noreply.github.com>
2025-01-11 13:19:28 +0300

    Update SearchTests.java

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -12,9 +12,8 @@ public class SearchTests {
     }
     @Test
     void successfulSearchTest1() {
-        open("https://ya.ru/");
         open("https://www.google.com/");
-        $x("//textarea[@name='q']").setValue("selenide").pressEnter();
+        $x("//textarea[@name='q']").setValue("selenide!!!!!!!"''').pressEnter();
         $x("//div[@id='search']").shouldHave(text("https://ru.selenide.org"));
     }
     @Test
