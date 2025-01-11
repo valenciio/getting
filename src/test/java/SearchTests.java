d9f7d8c24ee3fd47cadcc95391cd5cb502b6040f
Lenovo Gaming <selezenevva@krotech.ru>
2025-01-11 12:58:02 +0300

    chenges

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -16,9 +16,6 @@ public class SearchTests {
         $x("//textarea[@name='q']").setValue("selenide").pressEnter();
         $x("//div[@id='search']").shouldHave(text("https://ru.selenide.org"));
     }
-    private static String getMessage() {
-        return "selenide";
-    }
     @Test
     void successfulSearchTest2() {
         open("https://www.google.com/");
